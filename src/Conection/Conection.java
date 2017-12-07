e792b3c57667e96bc8461ae55b3b5c31af9a2144
Kevin Aguilar <kaguilara@ucenfotec.ac.cr>
2017-12-07 09:21:53 -0600

    fix

--- src/Conection/Conection.java
+++ src/Conection/Conection.java
@@ -5,10 +5,12 @@ package Conection;
  * @author kevin
  */
 
+import Classes.*;
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.io.*;
+import java.sql.CallableStatement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,29 +19,63 @@ public class Conection
 
     // Global variables
     static PrintStream out = System.out;
-    private final String database = "db_bank";
-    private final String user = "root";
-    private final String password = "teamwork";
-    private final String url = "jdbc:mysql://localhost:3306" + database;
-    private Connection con = null;
+    private static Connection con = null;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String database = "db_bank";
+    private static final String user = "root";
+    private static final String password = "teamwork";
+    private static final String url = "jdbc:mysql://localhost:3306" + database;
     
-    public Connection getConnection() 
-    {
+    
+    public static void main (String [ ] args) {
         
+    }
+    
+    private Connection getConnection() 
+    {
         try
         {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+            Class.forName(driver);
+            con = (Connection) DriverManager.getConnection(url, user, password);
             
         } catch(SQLException e)
         {
             out.println(e);
+            
         } catch (ClassNotFoundException ex) 
         {
-            Logger.getLogger(Conection.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return con;
+        return (Connection) con;
     }
+    
+    public void disconnect(){
+        con = null;
+        
+        if(con == null){
+            out.println("Connection close");
+        }
+    }
+    
+    public void saveUser(User pnewuser) throws SQLException
+    {
+        Connection conn = getConnection();
+        
+        String id = pnewuser.getId();
+        String name = pnewuser.getName();
+        String surname = pnewuser.getSurname();
+        String secondSurname = pnewuser.getSecondSurname();
+        String username = pnewuser.getUsername();
+        String email = pnewuser.getEmail();
+        String pasword = pnewuser.getPassword();
+        
+        String sp = "{call pa_post_new_user(id, name, surname, secondSurname, username, email, pasword)}";
+        
+        CallableStatement stpr = conn.prepareCall(sp);
+        
+    }
+
+
     
 }
--- src/Managers/ManagerUsers.java
+++ src/Managers/ManagerUsers.java
@@ -8,17 +8,40 @@ package Managers;
 import Conection.Conection;
 import java.io.*;
 import Classes.User;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ManagerUsers {
     
-    public static boolean registerUser (String[] newuser){
-        boolean validation = true;
+    public static String registerUser (String[] newuser) {
+        String validation;
+        Conection con = new Conection();
         
         //- User data
+        String id = newuser[0];
+        String name = newuser[1];
+        String surname = newuser[2];
+        String secondSurname = newuser[3];
+        String username = newuser[4];
+        String email = newuser[5];
+        String password = newuser[6];
         
-        User newUser = new User();
+        User newUser = new User(id,name,surname,secondSurname,username,email,password);
         
+        try {
+            con.saveUser(newUser);
+        } catch (SQLException ex) {
+            Logger.getLogger(ManagerUsers.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+        validation = "Usuario Guardado";
+                
         return validation;
+    }
+    
+    public static void getAllUsers()
+    {
+    
     }
 }
